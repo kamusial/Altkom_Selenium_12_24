ad950c98696707fbc156cf5ea3724726f2186b84
Your Name <you@example.com>
2024-12-19 11:22:52 +0100

    '1'

--- 9_POM/tests.py
+++ 9_POM/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from time import sleep
 from a2_selenium import make_screenshot
 import pytest
+import sys
 
 test_data = [
     ('standard_user', 'secret_sauce', 'https://www.saucedemo.com/inventory.html'),
@@ -11,22 +12,20 @@ test_data = [
     ('performance_glitch_user', 'secret_sauce', 'https://www.saucedemo.com/inventory.html')
 ]
 
-@pytest.mark.parametrize('username, password'),
-        [('standard_user','https://www.saucedemo.com/inventory.html'),
-         'locked_out_user',
-         'problem_user', 'performance_glitch_user'])
-def test_login_page(username):
+@pytest.mark.skipif(len('piesek') == 6, reason='not implemented')
+@pytest.mark.parametrize('username, password, url', test_data)
+def test_login_page(username, password, url):
     driver = webdriver.Firefox()
     page = LoginPage(driver)
     page.open()
     page.print_page_info()
     page.enter_username(username)
-    page.enter_password('secret_sauce')
+    page.enter_password(password)
     page.click_login()
     sleep(3)
     page.print_page_info()
     try:
-        assert page.get_current_url() == page.after_login_url
+        assert page.get_current_url() == url
     except AssertionError:
         print('Asercja nie przeszla')
         raise
@@ -36,3 +35,12 @@ def test_login_page(username):
         print('Koniec')
         make_screenshot(driver)
         page.close()
+
+if sys.platform.startswith("win"):
+    @pytest.mark.skip("skipping linux-only tests")
+    def test_default():
+        assert 3 == 4
+
+@pytest.mark.xfail(reason='not implemented')
+def test_default2():
+    assert 4 == 5
